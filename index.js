52c2432ee896f1541c149c9a473d20708dd9eeac
Dzaf96 <dzaf96@gmail.com>
2019-03-18 20:18:08 +0200

    create index.js and install nodemon

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app= express();
+const mongoose = require("mongoose");
+
+mongoose.connect('mongodb+srv://admin:admin@thesis-cluster-9doea.mongodb.net/test?retryWrites=true', { useNewUrlParser: true });
+
+app.get('/',(req,res)=>{
+    res.send('hello world!!!')
+});
+
+app.listen(3000, ()=>console.log("listening on port 3000"));

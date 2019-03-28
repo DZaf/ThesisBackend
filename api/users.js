d6b5de857d8d02ee18cd82ff7704089c1c10564d
gZogas <zogasgiorgos@windowslive.com>
2019-03-28 16:33:23 +0200

    check if user exists

--- api/users.js
+++ api/users.js
@@ -4,7 +4,9 @@ const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
 
-mongoose.connect('mongodb+srv://admin:admin@thesis-cluster-9doea.mongodb.net/test?retryWrites=true', { useNewUrlParser: true });
+mongoose.connect('mongodb+srv://admin:admin@thesis-cluster-9doea.mongodb.net/test?retryWrites=true', {
+    useNewUrlParser: true
+});
 
 // router.get('/',(req,res)=>{
 //     res.send('hello Users!!!')
@@ -16,10 +18,10 @@ router.get('/', (req, res) => {
         .select()
         .exec()
         .then(docs => {
-            const response ={
+            const response = {
                 count: docs.length,
                 users: docs.map(doc => {
-                    return{
+                    return {
                         _id: doc._id,
                         name: doc.name,
                         surname: doc.surname,
@@ -40,7 +42,15 @@ router.post('/register', (req, res) => {
 
     const result = validateUser(req.body); // Καλούμε την validateUser για να κάνουμε validate την είσοδο που παίρνουμε
     // ΑΝ ΤΟ ΠΟΤΕΛΕΣΜΑ ΕΧΕΙ ERROR ΤΟΤΕ ΕΠΙΣΤΡΕΦΟΥΜΕ 400
-    if(result.error) return res.status(400).send({success: 'false', message: result.error.details[0].message});
+    if (result.error) return res.status(400).send({
+        success: 'false',
+        message: result.error.details[0].message
+    });
+
+    if (checkUser(req.body.email)) return res.status(401).json({
+        success: 'false',
+        message: `user with email ${req.body.email} exists`
+    });
 
     const user = new User({ // Δημιουργούμε ένα νέο αντικείμενο User το οποίο θα μπει στην βάση και του δίνουμε τις τιμές από το request του client και το objectId που δημιουργεί αυτόματα η mongoose
         _id: new mongoose.Types.ObjectId(),
@@ -53,21 +63,24 @@ router.post('/register', (req, res) => {
     user // Κάνουμε save για να αποθηκευτεί στη βάση και μετά επιστρέφουμε κατάλληλο μήνυμα αν όλα πήγαν καλά και αντίστοιχα αν προέκυψε σφάλμα
         .save()
         .then(result => {
-            
+
             console.log(result);
             res.status(200).json({
-                success: 'true', 
-                message: 'OK', 
+                success: 'true',
+                message: 'User created successfully',
                 createdUser: result
-                });
-            })
+            });
+        })
         .catch(err => {
             console.log(err);
-            res.status(500).json({success: 'false', message: err});
+            res.status(500).json({
+                success: 'false',
+                message: err
+            });
         });
 });
 
-function validateUser (user){
+function validateUser(user) {
     // ΔΗΜΙΟΥΡΓΟΥΜΕ ΕΝΑ ΠΡΟΤΥΠΟ ΓΙΑ ΤΟ ΠΩΣ ΘΕΛΟΥΜΕ ΝΑ ΕΙΝΑΙ ΤΑ ΔΕΔΟΜΕΝΑ ΠΟΥ ΘΑ ΠΑΡΟΥΜΕ
     const schema = {
         // ΓΙΑ ΠΑΡΑΔΕΙΓΜΑ ΤΟ NAME ΠΡΕΠΕΙ ΝΑ ΕΧΕΙ ΤΟ ΛΙΓΟΤΕΡΟ 3 ΓΡΑΜΜΑΤΑ ΚΑΙ ΕΙΝΑΙ ΥΠΟΧΡΕΩΤΙΚΟ
@@ -80,4 +93,18 @@ function validateUser (user){
     return Joi.validate(user, schema);
 }
 
+function checkUser(user_email) {
+    User.findOne({
+        email: user_email
+    }, function (err, user) {
+        if (err) {
+            console.log(err);
+        }
+        if (user) {
+            return true;
+        } else {
+            return false;
+        }
+    });
+}
 module.exports = router;

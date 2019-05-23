ef85036a7b55962b41c61d977431ea70ab19e284
gZogas <zogasgiorgos@windowslive.com>
2019-05-23 15:34:37 +0300

    changes from local to git

--- crawlers/pw/pwcrawler.js
+++ crawlers/pw/pwcrawler.js
@@ -17,11 +17,13 @@ router.get('/', function (req, res, next) {
     res.send('respond programmable web crawler');
 });
 
-
+// First param is for starting page, second param is for final page
 router.get('/:0/:1', (req, res) => {
     startPage = req.params[0];
     stopPage = req.params[1];
-    console.log(startPage + " " + stopPage);
+    //console.log(startPage + " " + stopPage);
+
+    // crawl_per_page method call for retrieve api links from the range of the pages we give 
     crawl_per_page(startPage, stopPage).then((result) => {
         res.status(200).json({
             success: 'true',
@@ -32,19 +34,28 @@ router.get('/:0/:1', (req, res) => {
 
 });
 
-router.get('/page', (req, res) => {
+router.get('/page:start/:finish', (req, res) => {
     bigjson = {}
     bigjson["data"] = []
-
+    var counter = -1;
+    var start = req.params['start'];
+    var finish = req.params['finish'];
+    console.log(start);
+    console.log(finish);
+    // Links of the apis are given from the pwLinks.txt
     try {
         lineReader.eachLine('pwLinks.txt', function (line) {
-
-            crawl_page(line)/*.then((res) => {
-                //console.log(res);
-
-                
-
-            })*/
+            
+            counter++;
+            
+            console.log("counter : " + counter);
+            if (counter >= start)
+            {
+            // Line by line reading of the file and calling crawl_page method given the api link we take every time 
+            crawl_page(line);
+            }
+            if (counter == finish)
+            return false;
         })
     } catch (error) {
         console.log(error);
@@ -56,21 +67,10 @@ router.get('/page', (req, res) => {
         })
     }
 
-
-
-    // .then(function (err) {
-    //     if (err) throw err;
-    //     console.log("I'm done!!");
-    //     res.status(200).json({
-    //         success: 'true',
-    //         message: 'apis retrieved successfully'
-    //     })
-    // });
-
 });
 
 function crawl_page(link) {
-    //var url = "https://www.programmableweb.com" + link;
+    var url = "https://www.programmableweb.com" + link;
     // console.log(url);
     // return url;
     return puppeteer
@@ -79,15 +79,15 @@ function crawl_page(link) {
             return browser.newPage();
         })
         .then(function (page) {
-            return page.goto(link)
+            return page.goto(url)
                 .then(function () {
                     return page.content();
                 });
         })
         .then(function (html) {
             json = {}
-             json["API Title"] = $('.node-header h1', html)[0].children[0].data;
-            //console.log( $('.node-header h1', html)[0].children[0].data);
+            json["API Title"] = $('.node-header h1', html)[0].children[0].data;
+            json["API Description"] = $('.intro .api_description', html)[0].children[0].data;
             $('#myTabContent .specs .field', html).each(function () {
 
                 // SPAN LABEL
@@ -99,26 +99,22 @@ function crawl_page(link) {
                     json['' + $(this)[0].children[0].next.children[0].data] = $(this)[0].children[2].next.children[0].attribs.href;
                 }
             })
-            //return json;
 
-
-            const api = new Api({pwAPI:json});
-            //console.log(api);
-            //this.api = res;
+            const api = new Api({
+                pwAPI: json
+            });
 
             api // Κάνουμε save για να αποθηκευτεί στη βάση και μετά επιστρέφουμε κατάλληλο μήνυμα αν όλα πήγαν καλά και αντίστοιχα αν προέκυψε σφάλμα
                 .save()
                 .then(result => {
 
                     console.log(result);
-                 
+
                 })
                 .catch(err => {
                     console.log(err);
-                  
+
                 });
-
-
 
         })
         .catch(function (err) {
@@ -173,10 +169,10 @@ function crawl_per_page(page, upperlimit) {
 
 router.delete('/deleteCollection', (req, res) => {
 
-    mongoose.connection.db.dropCollection('apis', function(err, result) {
+    mongoose.connection.db.dropCollection('apis', function (err, result) {
         console.log(result)
     });
-    
-    });
 
-    module.exports = router;
+});
+
+module.exports = router;
